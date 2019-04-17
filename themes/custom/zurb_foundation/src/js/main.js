45b5a3b9d4105b7a9e0bcd23e02d8adf7fc5f896
oljasnurpeisov <oljasnurpeisov@gmail.com>
2019-04-17 14:47:22 +0600

    slider fix

--- themes/custom/zurb_foundation/src/js/main.js
+++ themes/custom/zurb_foundation/src/js/main.js
@@ -68,3 +68,89 @@
     }
   }
 })(jQuery, Drupal);
+
+(function ($, Drupal) {
+  Drupal.behaviors.FotoramaLightbox = {
+    attach: function (context, settings) {
+      function t() {
+          $(".fotorama").each(function() {
+              var t = $(this).data("fotorama").data;
+              $(this).find(".fotorama__stage__frame").each(function(e) {
+                  var o = $(this).width();
+                  img = t[e].img, 767 > o && t[e].sm && (img = t[e].sm), 510 > o && t[e].xs && (img = t[e].xs), $(this).children("img").attr("src", img)
+              })
+          })
+      }
+      $(function () {
+
+        var e = $(window),
+            o = $("body"),
+            i = function(a, t) {
+                a.data("open") ? a.stop().css({
+                    marginTop: -30,
+                    left: o.width() / 2 - a.width() / 2,
+                    top: e.scrollTop() + e.height() / 2 - a.height() / 2 - 20
+                }).animate({
+                    marginTop: 0,
+                    opacity: 1
+                }, t || 0) : a.css({
+                    left: -99999,
+                    top: -99999
+                })
+            },
+            s = function() {
+                var a = o.data("$fotorama");
+                a && (o.removeClass("overflow-hidden").data("$fotorama", null), a.stop().animate({
+                    marginTop: 30,
+                    opacity: 0
+                }, 300, function() {
+                    i(a.data("open", !1))
+                }))
+            };
+        $(".thumbs").each(function() {
+            var n = $(this),
+                r = $(".fotorama", n);
+            $fotorama = r.clone().addClass("fotorama-lightbox").data("allow-full-screen", "true").data("nav", "dots").data("arrows", "true").data("click", "true").appendTo("body").fadeTo(0, 0).fotorama(), fotorama = $fotorama.data("fotorama"), r.fotorama(), i($fotorama);
+            for (var l = 0, c = fotorama.data.length; c > l; l++) fotorama.data[l].id = fotorama.data[l].img;
+            $fotorama.on("click", function(a) {
+                a.stopPropagation()
+            }), n.on("click", ".fotorama__stage .fotorama__img", function(t) {
+                t.preventDefault(), t.stopPropagation();
+                var e = $(this),
+                    s = o.data("$fotorama");
+                fotorama.show({
+                    index: e.attr("src"),
+                    time: s ? 300 : 0,
+                    slow: t.altKey
+                }), fotorama.setOptions({
+                    height: "80%",
+                    maxwidth: "100%"
+                }), s || (o.addClass("overflow-hidden").data("$fotorama", $fotorama), $fotorama.css({
+                    left: e.offset().left,
+                    top: e.offset().top
+                }), i($fotorama.data("open", !0), 300))
+            }), t(), e.on("resize", function() {
+                i($fotorama), t()
+            }), $(document).on("click", s).on("keydown", function(a) {
+                var t = o.data("$fotorama");
+                if (t) {
+                    var e = t.data("fotorama");
+                    27 === a.keyCode ? (a.preventDefault(), s()) : 39 === a.keyCode ? e.show({
+                        index: ">",
+                        slow: a.altKey,
+                        direct: !0
+                    }) : 37 === a.keyCode && (a.preventDefault(), e.show({
+                        index: "<",
+                        slow: a.altKey,
+                        direct: !0
+                    }))
+                }
+            }), $fotorama.on("fotorama:fullscreenenter", function(a, t) {
+                t.cancelFullScreen(), s()
+            })
+        });
+
+      });
+    }
+  }
+})(jQuery, Drupal);
